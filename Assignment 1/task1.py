4fe38c1f823cc4b687e42773a5fa966331457e64
Domantas Baliukynas <domantas.baliukynas@mif.stud.vu.lt>
2022-02-19 17:49:29 +0200

    Make arguments not global

--- Assignment 1/task1.py
+++ Assignment 1/task1.py
@@ -4,13 +4,13 @@ import argparse
 import os
 import sys
 
-def print_sorted_groups(sorted_groups, limit_number):
+def print_sorted_groups(sorted_groups, order, limit_number):
     '''Print sorted log groups.
 
     Parameters
     ----------
-    sorted_groups : list
-        List of sorted groups in descending order.
+    sorted_groups : dictionary
+        Dictionary of sorted groups in descending order.
     limit_number: string / None
         Amount of rows to print.
     '''
@@ -27,9 +27,9 @@ def print_sorted_groups(sorted_groups, limit_number):
             print(f'{sorted_log}\n')
             limit_index+=1
 
-        if args.order == 'bytes':
+        if order == 'bytes':
             digit_count=len(str(sorted_logs['total_bytes']))
-        elif args.order == 'count':
+        elif order == 'count':
             digit_count=len(str(sorted_logs['count']))
         else:
             digit_count=len(str(sorted_logs['count_p']))
@@ -38,13 +38,13 @@ def print_sorted_groups(sorted_groups, limit_number):
         dash_symbols='-'  * digit_count
         equal_symbols='=' * digit_count
 
-        if args.order == 'bytes':
+        if order == 'bytes':
             print(f'''+------------{dash_symbols}+
 | TOTAL BYTES{space_symbols}|
 +============{ equal_symbols}+
 | {sorted_logs['total_bytes']}           |
 +------------{dash_symbols}+''')
-        elif args.order == 'count':
+        elif order == 'count':
             print(f'''+------{dash_symbols}+
 | COUNT{space_symbols}|
 +======{ equal_symbols}+
@@ -57,7 +57,7 @@ def print_sorted_groups(sorted_groups, limit_number):
 | {sorted_logs['count_p']}%               |
 +-----------------{dash_symbols}+''')
 
-def count_group_values(log_groups, log_list_len):
+def count_group_values(log_groups, log_list_len, order):
     '''Count grouped logs values.
 
     Parameters
@@ -66,32 +66,32 @@ def count_group_values(log_groups, log_list_len):
         Dictionary of logs that are grouped by IP address or HTTP status code.
     log_list_len: int
         Length of all logs.
-        
+
     Returns
     -------
     log_groups: dictionary
-        Dictionary of logs grouped by IP address or HTTP status code 
+        Dictionary of logs grouped by IP address or HTTP status code
         and its values (count, count_p or total_bytes).
     '''
     for logs in log_groups.values():
 
         total_bytes = 0
 
-        if args.order in ('count','count_p'):
+        if order in ('count','count_p'):
             count = len(logs['logs'])
             logs['count'] = count
 
-            if args.order == 'count_p':
+            if order == 'count_p':
                 logs['count_p'] = (logs['count'] / log_list_len) * 100
 
-        elif args.order == 'bytes':
+        elif order == 'bytes':
             for log in logs['logs']:
                 if log['size_in_bytes'] != '-' and log['size_in_bytes'] != '"-"\n':
                     total_bytes+=(log['size_in_bytes'])
             logs['total_bytes'] = total_bytes
     return log_groups
 
-def group_logs(log_list):
+def group_logs(log_list, group, order):
     '''Group logs by IP address or HTTP status code.
 
     Parameters
@@ -108,15 +108,15 @@ def group_logs(log_list):
     log_groups={}
 
     for log in log_list:
-        if args.group == 'ip':
+        if group == 'ip':
 
             if log['ip_address'] in log_groups:
                 log_groups[log['ip_address']]['logs'].append(log)
             else:
                 log_groups[log['ip_address']] = {'logs': [log]}
-                if args.order == 'bytes':
+                if order == 'bytes':
                     log_groups[log['ip_address']].setdefault("total_bytes", 0)
-                elif args.order in ('count', 'count_p'):
+                elif order in ('count', 'count_p'):
                     log_groups[log['ip_address']].setdefault("count", 0)
                 else:
                     log_groups[log['ip_address']].setdefault("count_p", 0)
@@ -125,9 +125,9 @@ def group_logs(log_list):
                 log_groups[log['status']]['logs'].append(log)
             else:
                 log_groups[log['status']] = {'logs': [log]}
-                if args.order == 'bytes':
+                if order == 'bytes':
                     log_groups[log['status']].setdefault("total_bytes", 0)
-                elif args.order in ('count', 'count_p'):
+                elif order in ('count', 'count_p'):
                     log_groups[log['status']].setdefault("count", 0)
                 else:
                     log_groups[log['status']].setdefault("count_p", 0)
@@ -183,18 +183,6 @@ def parse_log_file(log_file):
     return log_list
 
 def main():
-    with open(filename, 'r') as log_file:
-        if not os.path.getsize(filename):
-            print(f'File \"{filename.split("/").pop()}\" cannot be empty')
-            sys.exit()
-        log_list=parse_log_file(log_file)
-        log_list_len=len(log_list)
-        
-        log_groups=count_group_values(group_logs(log_list), log_list_len)
-        sorted_groups = dict(sorted(log_groups.items(), reverse=True))
-        print_sorted_groups(sorted_groups, args.limit)
-
-if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse Common Log Format file, '\
                                      'group logs by IP address or HTTP status code.')
     parser.add_argument('filename', help='A name of a non empty, CML format file')
@@ -207,6 +195,17 @@ if __name__ == '__main__':
     parser.add_argument('-limit','-l', help='Amount of rows to print')
     args = parser.parse_args()
 
-    filename = args.filename
+    with open(args.filename, 'r') as log_file:
+        if not os.path.getsize(args.filename):
+            print(f'File \"{args.filename.split("/").pop()}\" cannot be empty')
+            sys.exit()
+        log_list=parse_log_file(log_file)
+        log_list_len=len(log_list)
 
+        log_groups=count_group_values(group_logs(log_list, args.group, args.order),
+                                      log_list_len, args.order)
+        sorted_groups = dict(sorted(log_groups.items(), reverse=True))
+        print_sorted_groups(sorted_groups, args.order, args.limit)
+
+if __name__ == '__main__':
     main()

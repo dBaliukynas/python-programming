9a4b7435626672a00b4173b0ae1907d098ed9628
Domantas Baliukynas <domantas.baliukynas@mif.stud.vu.lt>
2022-03-26 17:54:39 +0200

    Add more unit tests

---
+++ Assignment 2/tests/test_task2.py
@@ -0,0 +1,95 @@
+import unittest
+import copy
+from tests import testdata
+from task2 import task2
+
+print(list(testdata.season1.games['Zalgiris Kaunas vs FC Barcelona'].count_performance_difference().values())[0])
+class TestSeason(unittest.TestCase):
+    def test_find_best_player_by_value_one_player(self):
+        result = testdata.season1.find_best_player_by_value('points')[0]
+        self.assertEqual(result, 
+        testdata.season1.teams['Zalgiris Kaunas'].players['Niels Giffey'])
+        
+    def test_find_best_player_by_value_list_length(self):
+        result = len(testdata.season1.find_best_player_by_value('points'))
+        self.assertEqual(result, 1)
+        
+    def test_find_best_player_by_value_multiple_players(self):
+        result = (testdata.season2.find_best_player_by_value('points')[0].points 
+                  == testdata.season2.find_best_player_by_value('points')[1].points)
+        self.assertTrue(result)
+        
+    def test_find_best_player_by_value_wrong_attribute(self):
+        self.assertRaises(AttributeError, lambda: 
+                          testdata.season1.find_best_player_by_value('nationality'))
+    
+    
+        
+    def test_find_highest_streak_team_one_team(self):
+        result = testdata.season2.find_highest_streak_team('win_streak')[0]
+        self.assertEqual(result, testdata.season2.teams['FC Bayern Munich'])
+        
+    def test_find_highest_streak_team_list_length(self):
+        result = len(testdata.season2.find_highest_streak_team('win_streak'))
+        self.assertEqual(result, 1)
+        
+    def test_find_highest_streak_team_multiple_teams(self):
+        result = (testdata.season1.find_highest_streak_team('win_streak')[0].win_streak
+        == testdata.season1.find_highest_streak_team('win_streak')[1].win_streak)
+        self.assertTrue(result)
+        
+    def test_find_highest_streak_team_wrong_attribute(self):
+        self.assertRaises(AttributeError, lambda: 
+                          testdata.season1.find_highest_streak_team('wins'))
+            
+class TestGame(unittest.TestCase):
+    def test_add_streak(self):
+        
+        team1_loss = testdata.season2.teams['FC Barcelona'].loss_streak
+        team2_win = testdata.season2.teams['Anadolu Efes Istanbul'].win_streak
+        
+        self.assertEqual(team1_loss, 1)
+        self.assertEqual(team2_win, 0)
+        
+        new_season2 = copy.deepcopy(testdata.season2)
+        task2.Game('Anadolu Efes Istanbul vs FC Barcelona', 29, 
+                      task2.TeamPerformance(new_season2.teams['Anadolu Efes Istanbul'], 108, 111, 44.2, 42.9,
+                                      85.4, 11, 17, 13, 13, 5, 12),
+                      task2.TeamPerformance(new_season2.teams['FC Barcelona'], 110, 94, 53.8, 40.0,
+                                      87.0, 11, 23, 20, 1, 0, 22 ))
+        
+        new_team1_loss = new_season2.teams['FC Barcelona'].loss_streak
+        new_team2_win = new_season2.teams['Anadolu Efes Istanbul'].win_streak
+        
+        self.assertEqual(new_team1_loss, 2)
+        self.assertEqual(new_team2_win, 1)
+        
+        task2.Game('Anadolu Efes Istanbul vs FC Barcelona', 29, 
+                      task2.TeamPerformance(new_season2.teams['Anadolu Efes Istanbul'], 108, 80, 44.2, 42.9,
+                                      85.4, 11, 17, 13, 13, 5, 12),
+                      task2.TeamPerformance(new_season2.teams['FC Barcelona'], 110, 94, 53.8, 40.0,
+                                      87.0, 11, 23, 20, 1, 0, 22 ))
+        
+        new_team1_loss = new_season2.teams['FC Barcelona'].loss_streak
+        new_team2_win = new_season2.teams['Anadolu Efes Istanbul'].win_streak
+        
+        self.assertEqual(new_team1_loss, 0)
+        self.assertEqual(new_team2_win, 0)
+        
+    def test_count_performance_difference(self):
+        result = list(testdata.season1.games['Zalgiris Kaunas vs FC Barcelona']
+                      .count_performance_difference().values())[0]
+        
+        expected_values = [-7, 5, -6, -1.6, 0, 37, 7, 12, 2.9, -10, -9.6]
+        for value, expected_value in zip(result.values(), expected_values):
+            self.assertEqual(value, expected_value)
+            
+        result = list(testdata.season1.games['FC Barcelona vs AS Monaco']
+                      .count_performance_difference().values())[0]
+        expected_values = [-9, 3, 2, 16.4, 0, 5, 19, 6, 12.7, -1, 2.4]
+        
+        for value, expected_value in zip(result.values(), expected_values):
+            self.assertEqual(value, expected_value)
+    
+if __name__ == '__main__':
+    unittest.main()
